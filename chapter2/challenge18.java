5809f4ab79878bb25aa45f9c25792cf1a06f07bb
GiveMeCoffeeOrGiveMeDeath <37058009+GiveMeCoffeeOrGiveMeDeath@users.noreply.github.com>
2024-01-27 17:32:00 -0500

    Update and rename challenge 18 to challenge18.java

--- chapter2/challenge18.java
+++ chapter2/challenge18.java
@@ -4,7 +4,7 @@ Chanllenge 18
 
 import java.util.Scanner;
 
-public class challenge18    {
+public class Main    {
     public static void main(String[]args)   {
         //declaring variables
         String name;
