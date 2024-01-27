056939493042098d917dbd27720d9c04b453d28a
GiveMeCoffeeOrGiveMeDeath <37058009+GiveMeCoffeeOrGiveMeDeath@users.noreply.github.com>
2024-01-27 17:25:30 -0500

    Update and rename Challenge11 to Challenge11.java

--- chapter3/Challenge11.java
+++ chapter3/Challenge11.java
@@ -19,32 +19,32 @@ class challenge11 {
         runner3 = keyboard.nextLine();
         time3 = keyboard.nextInt();
         
-        if(time1 > time2 && time3){
-            if(time2 > time3){
+      if (time1 > time2 && time1 > time3) {
+            if (time2 > time3) {
                 System.out.println("In order runner: " + runner1 + " with a time of " + time1);
                 System.out.println("In order runner: " + runner2 + " with a time of " + time2);
                 System.out.println("In order runner: " + runner3 + " with a time of " + time3);
-            }else{
+            } else {
                 System.out.println("In order runner: " + runner1 + " with a time of " + time1);
                 System.out.println("In order runner: " + runner3 + " with a time of " + time3);
                 System.out.println("In order runner: " + runner2 + " with a time of " + time2);
             }
-        } else if(time2 > time1 && time3){
-            if(time1 > time3){
+        } else if (time2 > time1 && time3 > time2) {
+            if (time1 > time3) {
                 System.out.println("In order runner: " + runner2 + " with a time of " + time2);
                 System.out.println("In order runner: " + runner1 + " with a time of " + time1);
                 System.out.println("In order runner: " + runner3 + " with a time of " + time3);
-            }else{
+            } else {
                 System.out.println("In order runner: " + runner2 + " with a time of " + time2);
                 System.out.println("In order runner: " + runner3 + " with a time of " + time3);
                 System.out.println("In order runner: " + runner1 + " with a time of " + time1);
             }
-        }else if(time3 > time2 && time1){
-            if(time2 > time1){
+        } else if (time3 > time2 && time1 > time3) {
+            if (time2 > time1) {
                 System.out.println("In order runner: " + runner3 + " with a time of " + time3);
                 System.out.println("In order runner: " + runner2 + " with a time of " + time2);
                 System.out.println("In order runner: " + runner3 + " with a time of " + time3);
-            }else{
+            } else {
                 System.out.println("In order runner: " + runner3 + " with a time of " + time3);
                 System.out.println("In order runner: " + runner1 + " with a time of " + time1);
                 System.out.println("In order runner: " + runner2 + " with a time of " + time2);
