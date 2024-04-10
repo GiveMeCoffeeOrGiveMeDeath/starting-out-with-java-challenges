7eb0f7a27df36c0d236e3f656a466f634ae75a42
GiveMeCoffeeOrGiveMeDeath <37058009+GiveMeCoffeeOrGiveMeDeath@users.noreply.github.com>
2024-04-10 09:53:20 -0400

    master

---
+++ chapter4/Challenge11.java
@@ -0,0 +1,16 @@
+/*Chapter 4 Challenge 11. Celcious to Fahrenheit table
+*/
+
+public class Challenge11   {
+   public static void main(String[]args)  {
+      double fahrenheit = 0.0;
+      int maxCelcius = 20;
+      
+      
+      for(int i = 0; i <= maxCelcius; i++){
+      fahrenheit = ((9/5)*(i)+ 32); 
+      
+      System.out.println("The Celcius " + i + " converted to Fahrenheit " + fahrenheit);
+      }
+   }
+}
