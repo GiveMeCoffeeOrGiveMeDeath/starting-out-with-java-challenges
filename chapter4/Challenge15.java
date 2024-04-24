16523109d36058c067b63ef5f310decf3283919d
GiveMeCoffeeOrGiveMeDeath <37058009+GiveMeCoffeeOrGiveMeDeath@users.noreply.github.com>
2024-04-24 15:09:53 -0400

    master

--- chapter4/Challenge15.java
+++ chapter4/Challenge15.java
@@ -10,6 +10,26 @@ public class Challenge15 {
       
       Scanner keyboard = new Scanner(System.in);
       
+      System.out.println("Enter the name of the first file: ");
+      file1 = keyboard.nextLine();
+      System.out.println("Enter the name of the second file: ");
+      file2 = keyboard.nextLine();
+      
+      File file = new File(file1);
+      Scanner inputFile = new Scanner(file1);
+      
+      PrintWriter outputFile = new PrintWriter(file2);
+      
+      if(file1.exists()){
+         System.out.println("Copying from file 1: ");
+      }
+      
+      
+      
+      inputFile.close();
+      output.close();
+      keyboard.close(); 
+      
        
    }
 }
