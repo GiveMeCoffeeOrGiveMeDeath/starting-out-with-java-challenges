1a06b4d9a69f1de3fb1af9b08929bc56ac5511f4
GiveMeCoffeeOrGiveMeDeath <37058009+GiveMeCoffeeOrGiveMeDeath@users.noreply.github.com>
2024-05-21 10:29:25 -0400

    master

--- chapter4/Challenge16.java
+++ chapter4/Challenge16.java
@@ -1,9 +1,39 @@
 /* Chapter 4 Challenge 16- Budget file converter
 */
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Challenge16   {
    public static void main(String[]args)  {
+      
+      double budget = 0;
+      double total = 0; 
+      double input = 0; 
+      double finalTotal = 0;
+      
       Scanner keyboard = new Scanner(System.in);
+      DecimalFormat df = new DecimalFormat("#.00");
+      
+      System.out.println("Enter the budget for the month:");
+      budget = keyboard.nextDouble();
+      
+      while(input != -99){
+         System.out.println("Enter -99 to exit; ");
+         System.out.println("Enter your expenses: ");
+         input = keyboard.nextDouble();
+         if(input > 0){
+            total += input;
+         }
+         
+         if(total > budget){
+            finalTotal = total - budget;
+         }else{
+            finalTotal = budget - total;
+         }
+      }
+      
+      System.out.println("The total amount for your month budget is: "+ df.format(budget) + " ,The total you use this month: " + df.format(total) + " ,after paying all expenses: " + df.format(finalTotal));
+      
+      keyboard.close();
    }
 }
