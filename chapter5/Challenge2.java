6e1a6e752e91e5f44a0861f7750717c4972e6cfd
David Acosta <david.ac.acosta10@gmail.com>
2024-06-13 19:28:48 -0400

    main

--- chapter5/Challenge2.java
+++ chapter5/Challenge2.java
@@ -1,29 +1,31 @@
 /* Chapter 5-  Challenge 2 - Retail Price Calculator 
 */
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class Challenge2 {
-   public static void main(String[]args)  {
-      
+   public static void main(String[] args) {
+
       double wholeSale = 0;
       int percent = 0;
       double total = 0;
       Scanner keyboard = new Scanner(System.in);
-      
+
       System.out.println("Enter the wholesale price of the product: ");
       wholeSale = keyboard.nextDouble();
       System.out.println("Enter the percentage ");
       percent = keyboard.nextInt();
-      
+
       System.out.println();
       total = calculateRetail(wholeSale, percent);
       System.out.println("Retail price: $" + total);
+
+      keyboard.close();
    }
-   
-   public static double calculateRetail(double wholeSale, int percent)   {
-   
-      double retailPrice = wholeSale * (wholeSale /100) + wholeSale;
+
+   public static double calculateRetail(double wholeSale, int percent) {
+
+      double retailPrice = wholeSale * (wholeSale / 100) + wholeSale;
       return retailPrice;
    }
 }
